ff36124a75f0513369a555ad9ae3067e8f80a30d
Jaya Soni <jayasoni19@gmail.com>
2025-06-18 12:39:34 +0530

    Updated all changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <h1>Welcome to Habot Service Request Form</h1>
+      <h1>Welcome to Service Request Form</h1>
       <h2>Click Next to Proceed..!!</h2>
       <button className='btn btn-primary' onClick={() => navigate('/ChildDetails')}>Next</button>
     </div>
